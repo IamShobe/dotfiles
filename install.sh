0b5aeb80c98c989d1c2cde449cecc92c5fc1ccf4
Elran Shefer <elran777@gmail.com>
2021-10-30 20:05:06 +0300

    Better installer

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ if ! [ -x "$(command -v ansible)" ]; then
   eval $run_as apt update
   eval $run_as apt install ansible -y
 fi
-ansible-playbook -i ./hosts ./dotfiles.yml --ask-become-pass
+[[ ! -e ~/.dotfiles ]] && git clone https://github.com/IamShobe/dotfiles ~/.dotfiles
+ansible-playbook -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --ask-become-pass
 if command -v terminal-notifier 1>/dev/null 2>&1; then
   terminal-notifier -title "dotfiles: Bootstrap complete" -message "Successfully set up dev environment."
 fi
