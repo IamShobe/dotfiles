c0b00a0596400c2db5fa5e7a62c5ec8ed4c2d47e
Elran Shefer <elran777@gmail.com>
2021-11-05 17:03:33 +0200

    Support macos

--- install.sh
+++ install.sh
@@ -1,13 +1,39 @@
 #!/usr/bin/env bash
 export run_as=""
 if [ -x "$(command -v sudo)" ]; then
-  export run_as="sudo"
+    export run_as="sudo"
+fi
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine = Mac ]; then
+    echo "Detected macos machine!"
+    if ! [ -x "$(command -v brew)" ]; then
+        echo "brew was not detected in machine.. installing..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "brew was detected!"
+    fi
+    brew install ansible
+
+elif [ $machine = Linux ]; then
+    echo "Detected linux based machine!"
+    if ! [ -x "$(command -v apt)" ]; then
+        echo "Currently only debian based distros are supported!"
+        exit 1
+    fi
+    if ! [ -x "$(command -v ansible)" ]; then
+      eval $run_as apt update
+      eval $run_as apt install ansible -y
+    fi
 fi
 set -e
-if ! [ -x "$(command -v ansible)" ]; then
-  eval $run_as apt update
-  eval $run_as apt install ansible -y
-fi
 [[ ! -e ~/.dotfiles ]] && git clone https://github.com/IamShobe/dotfiles ~/.dotfiles
 ansible-playbook -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --ask-become-pass
 if command -v terminal-notifier 1>/dev/null 2>&1; then
