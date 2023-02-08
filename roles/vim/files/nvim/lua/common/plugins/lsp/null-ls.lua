af1e84ddbc3254d9ef627fbd533f7e5fd76c15da
Elran Shefer <elran777@gmail.com>
2023-02-08 09:50:41 +0200

    fixup: disable prettier

--- roles/vim/files/nvim/lua/common/plugins/lsp/null-ls.lua
+++ roles/vim/files/nvim/lua/common/plugins/lsp/null-ls.lua
@@ -17,7 +17,7 @@ null_ls.setup({
   sources = {
     --  to disable file types use
     --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettier, -- js/ts formatter
+    -- formatting.prettier, -- js/ts formatter
     formatting.stylua, -- lua formatter
     diagnostics.eslint_d.with({ -- js/ts linter
       -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
