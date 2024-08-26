3a462f807d9db5d56eff18734cab621913287022
Elran Shefer <elran777@gmail.com>
2024-08-26 17:49:33 +0300

    fixup: initialize with chezmoi

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
+export run_as=""
+if [ -x "$(command -v sudo)" ]; then
+    export run_as="sudo"
+fi
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 
-# Install devbox
-curl -fsSL https://get.jetify.com/devbox | bash
 
-# Adds global chezmoi
-devbox global add chezmoi
+if [ $machine = Mac ]; then
+    echo "Detected macos machine!"
+    if ! [ -x "$(command -v brew)" ]; then
+        echo "brew was not detected in machine.. installing..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "brew was detected!"
+        # required for realpath
+        # brew install coreutils noti
+    fi
+
+elif [ $machine = Linux ]; then
+    echo "Detected linux based machine!"
+    if ! [ -x "$(command -v apt)" ]; then
+        echo "Currently only debian based distros are supported!"
+        exit 1
+    fi
+fi
+
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply IamShobe --branch chezmoi
+
