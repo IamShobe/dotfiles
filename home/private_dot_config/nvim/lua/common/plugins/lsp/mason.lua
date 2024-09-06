be48cdfb6cc5558c6db299cdf4158fd7caaf3e81
Elran Shefer <elran777@gmail.com>
2024-09-07 01:19:42 +0300

    Update home/.config/nvim/lua/common/plugins/lsp/mason.lua

--- home/private_dot_config/nvim/lua/common/plugins/lsp/mason.lua
+++ home/private_dot_config/nvim/lua/common/plugins/lsp/mason.lua
@@ -22,7 +22,7 @@ mason.setup()
 mason_lspconfig.setup({
   -- list of servers for mason to install
   ensure_installed = {
-    "ts_ls",
+    "tsserver",
     "html",
     "cssls",
     "tailwindcss",
